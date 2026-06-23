d5fff87120e2c8c42f433b0a1a41a5df61982182
Chehak7 <mongachehak@gmail.com>
2026-06-24 01:09:23 +0530

    Initial project setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./utils/connectDb.js"
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+  res.json({message: "Server is running"})
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+  connectDb()
+})
+
---
+++ server/models/user.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name :{type: String, required: true},
+    email :{type: String, required: true, unique: true},
+    credits :{type: Number, default: 150, minimum: 0},
+    isCreditAvailable :{type: Boolean, default: true},
+    notes : {type: [mongoose.Schema.Types.ObjectId], ref:"Notes", default:[]}
+
+},{timestamps: true})
+
+const UserModel = mongoose.model("UserModel", userSchema)
+
+export default UserModel
+
